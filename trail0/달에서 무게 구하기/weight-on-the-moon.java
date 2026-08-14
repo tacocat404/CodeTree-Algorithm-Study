4db3e73e915c7edd92d89614469062e7176dbc3b
tacocat404 <404tacocat404@gmail.com>
2026-08-14 12:09:43 +0900

    [Passed] 달에서 무게 구하기 (97ms, 9MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int c = 13;
+        double g = 0.165;
+        double o = 2.145000;
+        System.out.printf(c + " * %.6f = %.6f",g,o);
+    }
+}
