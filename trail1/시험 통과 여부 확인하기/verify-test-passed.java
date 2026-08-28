ddab59b1ea7647c0c23db18bad3feb2eb05c4e88
tacocat404 <404tacocat404@gmail.com>
2026-08-29 01:33:03 +0900

    [Passed] 시험 통과 여부 확인하기 (130ms, 12MB)

---
+++ trail1/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if (a >= 80) {
+            System.out.println("pass");
+        }
+        else {
+            System.out.println((80 - a) + " more score");
+        }
+    }
+}
