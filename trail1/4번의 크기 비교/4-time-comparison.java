28a7ba0c77906e1f02959a3bc58e1405bca4786b
tacocat404 <404tacocat404@gmail.com>
2026-09-01 20:07:08 +0900

    [Passed] 4번의 크기 비교 (130ms, 10MB)

---
+++ trail1/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+        int E = sc.nextInt();
+
+        System.out.println(A > B ? 1 : 0);
+        System.out.println(A > C ? 1 : 0);
+        System.out.println(A > D ? 1 : 0);
+        System.out.println(A > E ? 1 : 0);
+
+    }
+}
