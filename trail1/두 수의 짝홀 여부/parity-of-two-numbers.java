f3bcf10f34ccfc36534417d9afe670b435aff471
tacocat404 <404tacocat404@gmail.com>
2026-09-01 19:12:51 +0900

    [Passed] 두 수의 짝홀 여부 (150ms, 10MB)

---
+++ trail1/두 수의 짝홀 여부/parity-of-two-numbers.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (A % 2 == 0) {
+            System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
+
+        if (B % 2 == 0) {
+            System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
+    }
+}
