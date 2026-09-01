9e7eafe300b91140f30845e8121bb96b511c57b9
tacocat404 <404tacocat404@gmail.com>
2026-09-01 19:06:32 +0900

    [Passed] 두 번의 연산 (128ms, 10MB)

---
+++ trail1/두 번의 연산/two-operations.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        if (A % 2 == 1) {
+            A += 3;
+        }
+        if (A % 3 == 0) {
+            A /= 3;
+        }
+        System.out.println(A);
+    }
+}
