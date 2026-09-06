5e622084466a61fceebe2feefca50ec251dd1a0f
tacocat404 <404tacocat404@gmail.com>
2026-09-07 06:38:01 +0900

    [Passed] 학점 계산기 (150ms, 12MB)

---
+++ trail1/학점 계산기/credit-calculator.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double sumVal = 0;
+        double[] arr = new double[n];
+
+        for (int i = 0;i < n;i++) {
+            arr[i] = sc.nextFloat();
+            sumVal += arr[i];
+        }
+        double avg = (double)sumVal / n;
+        System.out.printf("%.1f\n",avg);
+
+        if (avg >= 4.0) {
+            System.out.println("Perfect");
+        }
+        else if (avg >= 3.0) {
+            System.out.println("Good");
+        }
+        else {
+            System.out.println("Poor");
+        }
+
+
+    }
+}
